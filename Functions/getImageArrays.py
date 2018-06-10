92219ceaac5441e955ee73bf4ce1a6357646bcf7
Daniel Dominic Longo <danieldominiclongo@gmail.com>
2018-06-10 03:08:28 -0700

    updates

--- Functions/getImageArrays.py
+++ Functions/getImageArrays.py
@@ -17,14 +17,19 @@ def getImageArrays(path, side_length, max_num_images): #returns list of images a
         if image_name.split(".")[-1] != "DS_Store":
             try:
                 cur_image_path = path + image_name
-                cur_image = np.array(ndimage.imread(cur_image_path,flatten=False))
-                cur_array_resized = scipy.misc.imresize(cur_image,size=(side_length,side_length))
-#                cur_array_flattened = cur_array_resized.reshape((side_length*side_length*3)).T
-#                examples += [cur_array_flattened] 
+                cur_image = np.array(ndimage.imread(cur_image_path, flatten=False))
+                cur_array_resized = scipy.misc.imresize(cur_image, size=(side_length, side_length))
+                assert(cur_array_resized.shape == (side_length, side_length, 3))
                 examples += [cur_array_resized]
+#                cur_array_flattened = cur_array_resized.reshape((side_length*side_length*3)).T
+#                examples += [cur_array_flattened]
+                # print("Cur array", np.shape(cur_array_resized)) 
             except ValueError:
-                print("Error in creating examples",image_name)
-    print("Examples shape", path, np.shape(examples))
+                print("Error in creating examples", image_name)
+            except AssertionError:
+                print("Invalid shape", image_name)
+            
+        # print("Examples shape", path, np.shape(examples))
     return examples
 
 def getExamples(side_length, image_path, test_ratio, max_num_images=-10):
@@ -34,7 +39,6 @@ def getExamples(side_length, image_path, test_ratio, max_num_images=-10):
 
     max_num_examples_cow = max_num_images // 2 
     max_num_examples_notCow = max_num_images - max_num_examples_cow
-    print(max_num_examples_notCow)
     examples_cow = getImageArrays(cow_images_path, side_length, max_num_examples_cow)
     print("examplesCow", np.shape(examples_cow))
     print(notCow_image_path, "herererer")
@@ -46,6 +50,13 @@ def getExamples(side_length, image_path, test_ratio, max_num_images=-10):
 
     examples = np.concatenate((examples_cow,examples_notCow))
     labels = np.concatenate((labels_cow,labels_notCow))
+    print("Examples Before", examples.shape)
+    print("labels Before", labels.shape)
+    shuffled_indexing = np.random.permutation(labels.shape[0])
+    examples = examples[shuffled_indexing]
+    labels = labels[shuffled_indexing]
+    print("Examples After", examples.shape)
+    print("labels After", labels.shape)
 
     assert(len(examples) == len(labels)), "labels and examples don't match"
     
@@ -63,16 +74,19 @@ def getExamples(side_length, image_path, test_ratio, max_num_images=-10):
     #reshape labels and examples for future matrix operations
     labels_train = np.reshape(labels_train,(1,len(labels_train)))
     labels_test = np.reshape(labels_test,(1,len(labels_test)))
-    examples_train = examples_train.T
-    examples_test = examples_test.T
+    examples_train = examples_train
+    examples_test = examples_test
 
      # Standardize color values of the image (decrease computational cost durring cross entropy)
     standardized_train_examples = examples_train/255 #225 is the maximum rgb value/ This is done to decrease varaince in inputs thus more efficint
     standardized_test_examples = examples_test/255
 #    print("Final Shapes:", "test:", standardized_test_examples.shape, "train:", standardized_train_examples.shape)
-    return standardized_train_examples, labels_train, standardized_test_examples, labels_test
+    return standardized_train_examples, labels_train.T, standardized_test_examples, labels_test.T
 
-x1,y1,x2,y2 = getExamples(side_length = 150,
-image_path = "../Data/Logistic_Regression_Data/",
-test_ratio = .3)
-print(x1.shape)
+# x1,y1,x2,y2 = getExamples(side_length = 150,
+# image_path = "../Data/Logistic_Regression_Data/",
+# test_ratio = .3)
+# print("X train", x1.shape)
+# print("Y train", y1.shape)
+# print("X test", x2.shape)
+# print("Y train", y2.shape)
